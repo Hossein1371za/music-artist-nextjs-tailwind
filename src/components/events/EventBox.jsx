1c8857c52fe0955b8aaf6d5a02c307db5e445c82
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-05-16 23:17:21 +0330

    varien framer motion

--- src/components/events/EventBox.jsx
+++ src/components/events/EventBox.jsx
@@ -1,7 +1,11 @@
+"use client"
+
 import Image from "next/image";
+import { RiMapPin2Fill } from "react-icons/ri";
+import {motion} from "framer-motion"
 
 const EventBox = ({ events }) => {
-    console.log(events.date);
+  console.log(events.date);
   return (
     <div className="bg-secondary/60 rounded-[10px] p-4 xl:p-12 relative">
       <div className="flex flex-col xl:flex-row justify-between h-[620px] xl:h-full gap-x-4">
@@ -15,19 +19,39 @@ const EventBox = ({ events }) => {
             alt=""
           />
         </div>
-        <div className="flex-1 bg-purple-400/10 h-[500px] flex flex-col">
-            {
-                events.map((event)=>{
-                    return <div key={event.id}>
-                        <div>
-                            <div>
-                                <div>{event.data.day}</div>
-                                <div>{event.data.mounth}</div>
-                            </div>
-                        </div>
+        <div className="flex-1 h-[500px] flex flex-col justify-between overflow-y-scroll scrollbar-thin scrollbar-thumb-accent scrollbar-track-white/10 xl:pr-6">
+          {events.map((event) => {
+            return (
+              <div
+                key={event.id}
+                className="flex flex-col xl:flex-row items-center justify-between gap-y-4 xl:gap-y-0 text-center xl:text-right my-4 xl:my-0 border-b border-white/10 py-10 xl:py-3 last-of-type:border-none"
+              >
+                <div className="flex flex-col xl:flex-row items-center gap-x-4">
+                  <div className="flex flex-col justify-center items-center leading-tight w-[80px] mb-4 xl:mb-0">
+                    <div className="text-[44px] font-black text-accent number">
+                      {event.data.day}
                     </div>
-                })
-            }
+                    <div className="text-[20px] font-extrabold">
+                      {event.data.mounth}
+                    </div>
+                  </div>
+                  <div className="w-64 flex flex-col gap-y-2">
+                    <div className="text-lg leading-none font-bold">{`${event.location.city}, ${event.location.country}`}</div>
+                    <div className="flex items-center gap-x-1 justify-center xl:justify-start">
+                      <div className="text-accent text-lg">
+                        <RiMapPin2Fill />
+                      </div>
+                      <div className="font-light">{event.location.address}</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-[100px] text-[17px] text-center text-accent font-bold">
+                  {event.priceRange}
+                </div>
+                <button className="btn btn-sm btn-accent xl:ml-4">خرید بلیط</button>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
