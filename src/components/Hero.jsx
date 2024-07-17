fe904be8ec49627a342104dc152fe54d1902bd9c
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-07-17 22:18:20 +0330

    hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,6 +25,25 @@ const Hero = () => {
   return (
     <section className="h-[80vh] xl:h-[850px]" id="home">
       <div className="container mx-auto h-full flex justify-center items-center xl:justify-start">
+        <div className="h-full flex flex-col justify-center items-center xl:items-start z-20 pt-12">
+          <MouseParallaxContainer
+            globalFactorX={0.1}
+            globalFactorY={0.2}
+            resetOnLeave
+            className="relative flex items-center h-[120px] xl:h-max xl:w-[840px]"
+          >
+            <MouseParallaxChild className="relative">
+              <div className="w-[300px] h-[101.37px] xl:w-[725px] xl:h-[244.97px]">
+                <Image
+                  src={"/assets/hero/typo-1.svg"}
+                  fill
+                  alt=""
+                  className="object-contain"
+                />
+              </div>
+            </MouseParallaxChild>
+          </MouseParallaxContainer>
+        </div>
         <div className="hidden xl:flex absolute left-0 top-0 before:w-[784px] before:h-[893px] before:absolute before:right-0 before:top-0 before:bg-singerOverlay before:z-10">
           <Image
             src={"/assets/hero/singer.png"}
@@ -35,7 +54,6 @@ const Hero = () => {
             priority
           />
         </div>
-        <div>text</div>
       </div>
     </section>
   );
