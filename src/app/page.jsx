be137857f06d47324d0124bfc2dd80eee7f363ac
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-07-17 18:50:38 +0330

    hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
 import Events from "@/components/events/Events";
+import Hero from "@/components/Hero";
 
 
 export default function Home() {
   return (
     <main>
+      <Hero/>
       <Events/>
     </main>
   );
---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Image from "next/image";
+import {
+  MouseParallaxContainer,
+  MouseParallaxChild,
+} from "react-parallax-mouse";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../varient";
+
+const locationSequence =[
+  'تهران , برج میلاد',3000,
+  'تهران , شهرک غرب',3000,
+  'تبریز , سالن همایش',3000,
+  'اصفهان , چهار باغ',3000,
+  'شیراز , حافظیه',3000,
+]
+const Hero = () => {
+  return <div>Hero</div>;
+};
+
+export default Hero;
