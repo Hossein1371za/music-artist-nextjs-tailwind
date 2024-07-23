1df42d5d6e258ab874eb4cbb4037260817c16a1f
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-07-23 20:00:26 +0330

    audio player & style

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,36 @@
-"use client"
+"use client";
 
+import Image from "next/image";
+import { AudioPlayer } from "react-audio-play";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../varient";
 
 const Player = () => {
   return (
-    <div>Player</div>
-  )
-}
+    <>
+      <div className="bg-gradient-to-r from-tertiary/70 to-primary/10 backdrop-blur-[15px]">
+        <div className="container mx-auto">
+          <div>متن</div>
+          <div>
+            <AudioPlayer
+              loop
+              preload="none"
+              color="#fff"
+              volume={40}
+              volumePlacement="top"
+              src="/assets/freedom.mp3"
+              style={{
+                background:"transparent",
+                boxShadow:"none",
+                width:"100%",
+                direction:"ltr",
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Player
+export default Player;
