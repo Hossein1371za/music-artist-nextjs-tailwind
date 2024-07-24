c8f41a33a3bd4e1367cf3b956911819469722588
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-07-24 18:53:11 +0330

    audio player style

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -8,10 +8,18 @@ import { fadeIn } from "../../varient";
 const Player = () => {
   return (
     <>
-      <div className="bg-gradient-to-r from-tertiary/70 to-primary/10 backdrop-blur-[15px]">
-        <div className="container mx-auto">
-          <div>متن</div>
-          <div>
+      <div className="bg-gradient-to-r from-tertiary/70 to-primary/10 backdrop-blur-[15px] h-[112px] flex items-center relative z-40">
+        <div className="container mx-auto flex flex-col justify-between items-center xl:flex-row">
+          <div className="hidden w-[300px] xl:flex items-center gap-x-4">
+            <div className="relative w-16 h-16">
+            <Image src={"/assets/player/avatar.png"} fill alt="" priority/>
+            </div>
+            <div>
+              <div className="text-lg font-medium">میا رینالدز</div>
+              <div className="text-sm font-light">آزادی</div>
+            </div>
+          </div>
+          <div className="w-full max-w-4xl">
             <AudioPlayer
               loop
               preload="none"
@@ -24,6 +32,7 @@ const Player = () => {
                 boxShadow:"none",
                 width:"100%",
                 direction:"ltr",
+                maxWidth:"100%"
               }}
             />
           </div>
