18f0eabe46bb018d09170a1d40bd3b3cdcfb112e
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-07-29 18:39:40 +0330

    end player section

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -8,7 +8,12 @@ import { fadeIn } from "../../varient";
 const Player = () => {
   return (
     <>
-      <div className="bg-gradient-to-r from-tertiary/70 to-primary/10 backdrop-blur-[15px] h-[112px] flex items-center relative z-40">
+      <motion.div
+      variants={fadeIn("up",0.1)}
+      initial="hidden"
+      whileInView={"show"}
+      viewport={{once:false,amount:0.1}}
+      className="bg-gradient-to-r from-tertiary/70 to-primary/10 backdrop-blur-[15px] h-[112px] flex items-center relative z-40">
         <div className="container mx-auto flex flex-col justify-between items-center xl:flex-row">
           <div className="hidden w-[300px] xl:flex items-center gap-x-4">
             <div className="relative w-16 h-16">
@@ -37,7 +42,7 @@ const Player = () => {
             />
           </div>
         </div>
-      </div>
+      </motion.div>
     </>
   );
 };
