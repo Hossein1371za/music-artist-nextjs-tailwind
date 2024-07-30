f121b8e4bdea033db9d5990112f0d3ab33fefeb5
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-07-30 18:35:00 +0330

    scroll header-bg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,40 @@
-"use client"
-import { useEffect,useState } from "react"
-import Image from "next/image"
-import Link from "next/link"
+"use client";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-
-import {motion} from "framer-motion"
-import { fadeIn } from "../../varient"
+import { motion } from "framer-motion";
+import { fadeIn } from "../../varient";
 
 const Header = () => {
+  const [active, setActive] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      setActive(window.scrollY > 100);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header>
+    <header
+      className={`fixed z-50 w-full ${
+        active ? "bg-[#030315] py-6" : "bg-transparent py-8"
+      } `}
+    >
       <div className="container mx-auto">
-        <Link href={"#"} className="relative flex w-[226px] h-[37.64px]">
+        <Link
+          href={"#"}
+          className="relative flex w-[226px] h-[37.64px] transition-all mb-4 xl:mb-0"
+        >
           <Image src={"/assets/header/logo.svg"} fill />
         </Link>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
