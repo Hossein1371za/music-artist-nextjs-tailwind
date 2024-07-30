6b629b3bd0030f092aa84662c2171b8e310e1d8a
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-07-30 18:13:34 +0330

    header nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
+"use client"
+import { useEffect,useState } from "react"
+import Image from "next/image"
+import Link from "next/link"
+
+
+import {motion} from "framer-motion"
+import { fadeIn } from "../../varient"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container mx-auto">
+        <Link href={"#"} className="relative flex w-[226px] h-[37.64px]">
+          <Image src={"/assets/header/logo.svg"} fill />
+        </Link>
+      </div>
+    </header>
   )
 }
 
