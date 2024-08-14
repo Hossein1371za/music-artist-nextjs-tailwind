995a0e8419c643680076d0e180cc091fce805c98
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-08-14 22:18:44 +0330

    nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,18 +2,26 @@ import { Link } from "react-scroll";
 
 const links = [
   { path: "home", name: "خانه" },
-  { path: "tours", name: "مکان ها" },
+  { path: "tours", name: "تور ها" },
   { path: "discography", name: "فهرست ها" },
   { path: "contact", name: "ارتباط با ما" },
 ];
 
 const Nav = ({ containerStyles, linkStyles }) => {
   return (
-    <nav>
-      {links.map((link,inx) => (
-        <Link key={inx} to={link.path}>
+    <nav className={`${containerStyles}`}>
+      {links.map((link, inx) => (
+        <Link
+          key={inx}
+          className={`${linkStyles} cursor-pointer border-b-2 border-transparent`}
+          to={link.path}
+          smooth
+          spy
+          offset={-50}
+          activeClass="active"
+        >
           {link.name}
-        </Link>
+        </Link> 
       ))}
     </nav>
   );
