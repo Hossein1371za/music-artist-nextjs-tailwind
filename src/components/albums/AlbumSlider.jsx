aebcc26ff8ed3608f568cebeaf1678c3862c1244
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-08-18 22:32:06 +0330

    slider

--- src/components/albums/AlbumSlider.jsx
+++ src/components/albums/AlbumSlider.jsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 
 import { AudioPlayer } from "react-audio-play";
 
-import { Swiper, swiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide, swiperSlide } from "swiper/react";
 import { EffectCoverflow, FreeMode, Navigation, Thumbs } from "swiper/modules";
 
 import "swiper/css";
@@ -20,9 +20,34 @@ const AlbumSlider = () => {
   const { data, error } = useSWR("http://localhost:4000/albums", fetcher);
 
   if (error) return "دسترسی به اطلاعات امکان پذیر نمیباشد!";
-  if (!data) return "در حال بارگزاری";
+  if (!data) return "...در حال بارگزاری";
 
-  return <div>AlbumSlider</div>;
+  return <>
+  <Swiper>
+    {data.map((album)=>{
+      return <SwiperSlide key={album.id} className="mb-12">
+        <div className="w-full bg-secondary/80 rounded-[10px] flex flex-col xl:flex-row items-center p-6 xl:p-12 gap-x-12">
+          <div className="hidden xl:flex w-[300px] h-[300px] xl:w-[500px] xl:h-[500px] relative cursor-pointer rounded-[10px] overflow-hidden">
+            <Image 
+            src={album.img}
+            fill
+            priority
+            alt=""
+            className="object-contain"
+            />
+          </div>
+          <div>
+            <div>
+              {album.tracks?.map((track,index)=>{
+                return <div key={index}>{track.name}</div>
+              })}
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+    })}
+  </Swiper>
+  </>;
 };
 
 export default AlbumSlider;
