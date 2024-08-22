10f8af2867ae94955c46fb671351591bdd87fa0f
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-08-22 19:36:14 +0330

    end of blog section

--- src/components/blog/PostList.jsx
+++ src/components/blog/PostList.jsx
@@ -9,9 +9,14 @@ import { fadeIn } from "../../../varient";
 const PostList = ({ posts }) => {
   const firstThreePost = posts.slice(0, 3);
 
-
   return (
-    <div>
+    <motion.div
+      variants={fadeIn("up", 0.4)}
+      initial="hidden"
+      whileInView={"show"}
+      viewport={{ once: false, amount: 0.3 }}
+      className="flex flex-col items-center"
+    >
       <div className="flex flex-col xl:flex-row justify-between gap-12 py-10 xl:pb-24 border-t border-white/10">
         {firstThreePost.map((post) => {
           const { id, date, title, descripton } = post;
@@ -21,14 +26,15 @@ const PostList = ({ posts }) => {
               <div className="text-xl font-medium mb-4">{title}</div>
               <div className="text-white/30 mb-6 font-light">{descripton}</div>
               <Link href="#" className="flex item-center gap-x-2 group">
-              ادامه مطلب
-              <BsArrowLeft className="text-xl group-hover:mr-1 transition-all "/>
+                ادامه مطلب
+                <BsArrowLeft className="text-xl group-hover:mr-1 transition-all " />
               </Link>
             </div>
           );
         })}
       </div>
-    </div>
+      <button className="btn btn-lg btn-accent">تماشای همه پست ها</button>
+    </motion.div>
   );
 };
 
